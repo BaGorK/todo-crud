ad2e13b0f033cf94c3816928a1786f1f2ce42bda
BaGorK <edmealemkassahun@gmail.com>
2024-03-20 22:58:20 +0300

    feat: basic express setup

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hello');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Task manager App!');
+});
+
+const port = 3000;
+
+app.listen(port, console.log(`Server is listening on port ${port}`));
