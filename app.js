d239a75a1cb6c30446e568dc73ab017f858bedbc
BaGorK <edmealemkassahun@gmail.com>
2024-03-21 23:40:31 +0300

    feat: setting controllers and routes | getAllTasks

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const taskRoute = require('./routes/tasks');
 
 const app = express();
 
+// middleware
+app.use(express.json());
+
+// routes
 app.get('/', (req, res) => {
   res.send('Task manager App!');
 });
 
+app.use('/api/v1/tasks', taskRoute);
+
 const port = 3000;
 
+// server
 app.listen(port, console.log(`Server is listening on port ${port}`));
