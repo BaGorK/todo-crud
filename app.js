28e0cacfe0d25addcb1034f1ffd7b81b74bdde25
BaGorK <edmealemkassahun@gmail.com>
2024-03-26 00:55:38 +0300

    feat: handling undefined routes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const taskRouter = require('./routes/tasksRoute');
+const notFound = require('./middleware/not-found');
 const app = express();
 
 // middleware
@@ -8,5 +9,6 @@ app.use(express.json());
 
 // routes
 app.use('/api/v1/tasks', taskRouter);
+app.use(notFound);
 
 module.exports = app;
